79b831166a89c1a8d1dcb5282b30d1669954bc75
denis <denis@DESKTOP-G37V40S>
2022-02-10 05:12:21 +0000

    Add project files.

---
+++ montecarlo/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace montecarlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCircleCount = new System.Windows.Forms.Label();
+            this.lblSquareCount = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPiApprox = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Circle: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-5, 472);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Square: ";
+            // 
+            // lblCircleCount
+            // 
+            this.lblCircleCount.AutoSize = true;
+            this.lblCircleCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCircleCount.Location = new System.Drawing.Point(97, 441);
+            this.lblCircleCount.Name = "lblCircleCount";
+            this.lblCircleCount.Size = new System.Drawing.Size(29, 31);
+            this.lblCircleCount.TabIndex = 3;
+            this.lblCircleCount.Text = "0";
+            // 
+            // lblSquareCount
+            // 
+            this.lblSquareCount.AutoSize = true;
+            this.lblSquareCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSquareCount.Location = new System.Drawing.Point(97, 472);
+            this.lblSquareCount.Name = "lblSquareCount";
+            this.lblSquareCount.Size = new System.Drawing.Size(29, 31);
+            this.lblSquareCount.TabIndex = 4;
+            this.lblSquareCount.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 550);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Pi approx: ";
+            // 
+            // lblPiApprox
+            // 
+            this.lblPiApprox.AutoSize = true;
+            this.lblPiApprox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPiApprox.Location = new System.Drawing.Point(6, 583);
+            this.lblPiApprox.Name = "lblPiApprox";
+            this.lblPiApprox.Size = new System.Drawing.Size(29, 31);
+            this.lblPiApprox.TabIndex = 6;
+            this.lblPiApprox.Text = "0";
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(427, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(1306, 733);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1745, 757);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.lblPiApprox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSquareCount);
+            this.Controls.Add(this.lblCircleCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCircleCount;
+        private System.Windows.Forms.Label lblSquareCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPiApprox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ montecarlo/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+
+namespace montecarlo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         
+            panel1.Paint += new PaintEventHandler(panel1_Paint);
+            chart1.ChartAreas[0].AxisY.ScaleView.MinSize = 1;
+            chart1.ChartAreas[0].CursorY.Interval = 0.00001;
+        }
+
+       
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+
+        {
+            Random rand = new Random();
+            Pen pen = new Pen(Color.Black, (float)2);
+           
+            Graphics g = e.Graphics;
+            int count = 0;
+            int square = 0;
+            int circle = 0;
+            double pi;
+                g.DrawRectangle(pen, 0, 0, 400, 400);
+                g.DrawEllipse(pen, 0,0, 400, 400);
+            chart1.Series.Add("Pi");
+            chart1.Series[ "Pi"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+           
+            System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation PiY = new System.Windows.Forms.DataVisualization.Charting.HorizontalLineAnnotation();
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].CursorY.AutoScroll = true;
+            PiY.AxisY = chart1.ChartAreas[0].AxisY;
+            PiY.AxisX = chart1.ChartAreas[0].AxisX;
+            PiY.IsSizeAlwaysRelative = false;
+            PiY.AnchorY = 3.141592;
+            PiY.IsInfinitive = true;
+            PiY.ClipToChartArea = chart1.ChartAreas[0].Name;
+            PiY.LineColor = Color.Red; 
+            PiY.LineWidth = 1;
+            chart1.Annotations.Add(PiY);
+            List<double> xAxis = new List<double>();
+            List<double> yAxis = new List<double>();
+            yAxis.Add(3.1415);
+            for (int i = 0; i < 50000; i++)
+            {
+                count++;
+                xAxis.Add(count);
+                int x = rand.Next(0, 400);
+                int y = rand.Next(0, 400);
+                if ((x - 200) * (x - 200) +  (y - 200) * (y - 200) <= 200 * 200)
+                {
+                    pen.Color = Color.Red;
+                    circle++;
+
+                }
+                else if (x>=0&& x <=400 &&y>=0 && y<=400)
+                {
+                    pen.Color = Color.Blue;
+                    square++;
+                }
+                else
+                {
+                    pen.Color = Color.Black;
+                }
+                lblSquareCount.Text = Convert.ToString(square);
+                lblCircleCount.Text = Convert.ToString(circle);
+                pi = (double)4 * circle / (square + circle);
+                lblPiApprox.Text = Convert.ToString(pi);
+                yAxis.Add(count);
+                lblSquareCount.Update();
+                lblCircleCount.Update();
+                lblPiApprox.Update();
+                g.DrawEllipse(pen,x,y,1,1);
+                chart1.Series["Pi"].Points.AddXY(count, pi);
+                if (count % 50==0)
+                {
+                chart1.Update();
+                }
+            }
+            chart1.Update();
+        }
+       
+    }
+}
